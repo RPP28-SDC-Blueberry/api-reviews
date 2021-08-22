e81e24c41efd1620c3f084992d88ff5b7632d2ab
Bill Roth <minongcopper@gmail.com>
2021-08-21 22:36:02 -0400

    add redis to products route

--- server/app.js
+++ server/app.js
@@ -7,15 +7,40 @@ app.get('/', (req, res) => {
   res.status(418).send('Hello, there, World on Thursday!');
 });
 
+//Redis
+let client = require('redis').createClient(redis_url);
+let Redis = require('ioredis');
+let redis = new Redis('redis://172.31.62.219:6379');
 
 app.get('/products', (req, res) => {
-  db.products(req.query.page, req.query.count, (error, data) => {
-    if (error) {
-      res.sendStatus(502);
-    } else {
-      res.status(200).json(data);
+
+  const uid = `products_page${req.query.page.toString()}_count${req.query.count.toString()}`; //uid is unique identifier
+  //check if rep details are present in cache
+  client.get(uid, (error, rep)=> {
+    if(error){
+      console.log('Query could not be fetched from Redis. Error:', error);
+    }
+    if(rep){
+    //JSON objects need to be parsed after reading from redis, since it is stringified before being stored into cache
+
+      res.status(200).json(JSON.parse(rep));
+    }
+    else{
+      db.products(req.query.page, req.query.count, (error, data) => {
+        if (error) {
+          res.sendStatus(502);
+        } else {
+          res.status(200).json(data);
+          //cache data received from db
+          client.set(uid, JSON.stringify(response_data),(error, result)=> {
+          if(error){
+            console.log('Data not written to Redis. Error:', error);
+          }})
+        }
+      })
     }
   })
+
 });
 
 app.get('/products/:product_id', (req, res) => {
