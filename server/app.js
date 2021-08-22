ef33cc0c00737c8b2b24f0cbcf71f6032047ba6e
Bill Roth <minongcopper@gmail.com>
2021-08-21 22:49:54 -0400

    update redis url

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 //Redis
 let client = require('redis').createClient(redis_url);
 let Redis = require('ioredis');
-let redis = new Redis('redis://172.31.62.219:6379');
+let redis = new Redis('172.31.62.219:6379');
 
 app.get('/products', (req, res) => {
 
